bbdf8eb1db326a28f33cd61af9a1689b1cf2d490
erdem <erdemozkol@gmail.com>
2024-05-26 19:35:16 +0100

    FastApi app runs with singleton config implementation

---
+++ src/config.py
@@ -0,0 +1,51 @@
+import logging
+import os
+import pathlib
+from functools import lru_cache
+
+from dotenv import dotenv_values
+from pydantic_settings import BaseSettings
+
+
+class BaseConfig(BaseSettings):
+    BASE_DIR: pathlib.Path = pathlib.Path(__file__).parent.parent
+    ASSETS_DIR: pathlib.Path = BASE_DIR / pathlib.Path("assets")
+    DEBUG: bool
+    ENV: str = os.environ.get("FASTAPI_ENV", "development")
+    APP_NAME: str
+    PDF_SERVICE_API_KEY: str
+    LOGGING_LEVEL: int = logging.INFO
+
+    @property
+    def is_production(self):
+        return self.ENV == "production"
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG: bool = True
+    LOGGING_LEVEL: int = logging.DEBUG
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG: bool = False
+
+
+class TestingConfig(BaseConfig):
+    DEBUG: bool = False
+
+
+@lru_cache()
+def get_settings():
+    config_cls_dict = {
+        "development": DevelopmentConfig,
+        "production": ProductionConfig,
+        "testing": TestingConfig,
+    }
+
+    env_dict = dotenv_values(dotenv_path=".env")
+    config_name = env_dict.pop("FASTAPI_ENV", "development")
+    config_cls = config_cls_dict[config_name]
+    return config_cls(**env_dict)
+
+
+settings = get_settings()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,43 @@
+import logging
+from typing import Callable, List, Dict
+
+import pandas as pd
 from fastapi import FastAPI
 
-app = FastAPI()
+from .config import settings
+from .pdf_service import PdfService
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+logging.basicConfig(level=settings.LOGGING_LEVEL)
+logger = logging.getLogger(__name__)
+
+_app_init_hooks: list[Callable[[FastAPI], None]] = []
+app_init_hook = _app_init_hooks.append
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG)
+    for f in _app_init_hooks:
+        f(app)
+    return app
+
+
+app = create_app()
+
+
+@app_init_hook
+def _validate_pdf_service_credentials(app: FastAPI) -> None:
+    try:
+        PdfService(settings.PDF_SERVICE_API_KEY)
+    except AssertionError as e:
+        raise RuntimeError("Invalid PDF service credentials") from e
+
+
+def get_pdf_service() -> PdfService:
+    return PdfService(settings.PDF_SERVICE_API_KEY)
+
+
+def get_database_csv() -> List[Dict[str, str]]:
+    df = pd.read_csv("data/database.csv")
+    data = df.to_dict(orient="records")
+    return data
